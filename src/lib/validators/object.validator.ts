fb3eedf9067b2981cac78bbdd604be51f65539ae
Karol Rodziński <karolrodzinski@gmail.com>
2022-03-14 19:57:06 +0100

    Update README.md with short manual

--- src/lib/validators/object.validator.ts
+++ src/lib/validators/object.validator.ts
@@ -17,27 +17,18 @@ export const ObjectValidator: Validator<ObjectSchema> = {
       throw new ObjectValidationError("Provided value is an array");
     }
 
-    /*
-      Check if all of properties in data are defined in schema 
-    */
     if (schema?.properties) {
       if (!Object.keys(data).every((propertyName) => Object.keys(schema?.properties).includes(propertyName))) {
         throw new ObjectValidationError("Schema does not allow some of the properties");
       }
     }
 
-    /*
-      Check if all of the required properties in schema are defined in data 
-    */
     if (schema?.required) {
       if (!schema?.required.every((requiredProperty) => Object.keys(data).includes(requiredProperty))) {
         throw new ObjectValidationError("ERROR");
       }
     }
 
-    /*
-      Validate each property - 'recurring' validation. 
-    */
     let validationErrors = [];
     Object.keys(data).forEach((property) => {
       try {
