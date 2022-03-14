ffba181a1bc6cdd54ae67ed6dbbc8e4faaaaa7db
Karol Rodziński <karolrodzinski@gmail.com>
2022-03-14 15:30:17 +0100

    String validator implementation with basic testing

--- src/lib/validators/string.validator.ts
+++ src/lib/validators/string.validator.ts
@@ -1,8 +1,18 @@
 import type { StringSchema } from "../schema";
+import { StringValidationError } from "../errors";
 import { Validator } from "./validator.interface";
 
 export const StringValidator: Validator<StringSchema> = {
   validate: (data, schema) => {
+    if (typeof data !== "string") {
+      throw new StringValidationError(`Type of the value is not 'string'`);
+    }
+    /*
+      TODO: What to return if an enum is an empty array - should we allow all of the values or none of them?
+    */
+    if (schema?.enum && !schema?.enum?.includes(data)) {
+      throw new StringValidationError(`Value is not found in defined enum`);
+    }
     return true;
   },
 };
--- src/test/string-validator.test.ts
+++ src/test/string-validator.test.ts
@@ -1,30 +1,89 @@
 import type { StringSchema } from "../lib/schema";
+import { StringValidationError } from "../lib/errors";
 import { StringValidator } from "../lib/validators";
 
-describe("String schema validator", () => {
-  let schema: StringSchema = {
+describe("String validator", () => {
+  const schema: StringSchema = {
     type: "string",
   };
+
+  describe("Valid", () => {
+    test("Simple string", () => {
+      const data = "h1, h2";
+
+      expect(StringValidator.validate(data, schema)).toBe(true);
+    });
+
+    test("Empty string", () => {
+      const data = "";
+
+      expect(StringValidator.validate(data, schema)).toBe(true);
+    });
+  });
+
+  describe("Invalid", () => {
+    test("Boolean", () => {
+      const data = false;
+
+      expect(() => StringValidator.validate(data, schema)).toThrowError(StringValidationError);
+    });
+
+    test("Number", () => {
+      const data = 10;
+
+      expect(() => StringValidator.validate(data, schema)).toThrowError(StringValidationError);
+    });
+
+    test("Symbol", () => {
+      const data = Symbol("h1");
+
+      expect(() => StringValidator.validate(data, schema)).toThrowError(StringValidationError);
+    });
+
+    test("Array", () => {
+      const data = ["aaa", "aaa"];
+
+      expect(() => StringValidator.validate(data, schema)).toThrowError(StringValidationError);
+    });
+
+    test("Object", () => {
+      const data = { property: "h1" };
+
+      expect(() => StringValidator.validate(data, schema)).toThrowError(StringValidationError);
+    });
+  });
+});
+
+describe("String validator with enum", () => {
   let schemaWithEnum: StringSchema = {
     type: "string",
     enum: ["h1", "h2", "h3", "h4"],
   };
 
-  test("Empty string", () => {
-    const data = "";
+  let schemaWithEmptyEnum: StringSchema = {
+    type: "string",
+    enum: [],
+  };
 
-    expect(StringValidator.validate(data, schema)).toBe(true);
+  describe("Valid", () => {
+    test("Enum", () => {
+      const data = "h1";
+
+      expect(StringValidator.validate(data, schemaWithEnum)).toBe(true);
+    });
   });
 
-  test("Enum", () => {
-    const data = ["h1", "h2"];
+  describe("Invalid", () => {
+    test("Empty enum", () => {
+      const data = "h1";
 
-    expect(StringValidator.validate(data, schema)).toBe(true);
-  });
+      expect(() => StringValidator.validate(data, schemaWithEmptyEnum)).toThrow(StringValidationError);
+    });
 
-  test("Number", () => {
-    const data = 10;
+    test("Invalid value form Enum", () => {
+      const data = "h100";
 
-    expect(StringValidator.validate(data, schema)).toBe(false);
+      expect(() => StringValidator.validate(data, schemaWithEnum)).toThrow(StringValidationError);
+    });
   });
 });
